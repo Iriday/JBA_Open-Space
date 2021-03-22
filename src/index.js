323eeb8b78e9a91f0ab96a83430d3f4b48348fa3
Igor <GI_Something@protonmail.com>
2021-03-22 09:56:45 +0200

    feat: The launch btn is locked until all controls positioned correctly

--- src/index.js
+++ src/index.js
@@ -1,26 +1,68 @@
 const PASSWORD = "TrustNo1";
 const unlockBtn = document.getElementById("unlock-btn");
 const passField = document.getElementById("password-field");
-const controlPanel = document.getElementsByClassName("control-panel")[0]
-let panelLocked = true;
+const lunchBtn = document.getElementById("launch-btn");
+const checkBtns = document.getElementsByClassName("check-buttons")[0];
+const levers = document.getElementsByClassName("levers")[0];
 
-function togglePanelLock() {
-    controlPanel.querySelectorAll("input")
-        .forEach(value => {
-            if (value.getAttribute("id") === "password-field"
-                || value.getAttribute("id") === "unlock-btn") {
-                value.disabled = !panelLocked
-            } else {
-                value.disabled = panelLocked
-            }
-        });
-    panelLocked = !panelLocked;
+function setDisabled(inputs, desabled = false) {
+    const f = input => input.disabled = desabled
+
+    if (inputs.tagName.toUpperCase() === "INPUT") {
+        f(inputs)
+    } else {
+        inputs.querySelectorAll("input")
+            .forEach(f)
+    }
 }
 
-unlockBtn.onclick = () => {
-    if (passField.value === PASSWORD) {
-        togglePanelLock();
+function isReadyToLunch() {
+    for (const e of checkBtns.getElementsByTagName("input")) {
+        if (!e.checked) return false;
     }
-};
 
-togglePanelLock();
+    for (const e of levers.getElementsByTagName("input")) {
+        if (e.value < 100) return false;
+    }
+
+    return true;
+}
+
+function initCheckBtns() {
+    for (const e of checkBtns.getElementsByTagName("input")) {
+        e.onchange = () => {
+            lunchBtn.disabled = !isReadyToLunch();
+        }
+    }
+}
+
+function initLevers() {
+    for (const e of levers.getElementsByTagName("input")) {
+        e.onchange = () => {
+            lunchBtn.disabled = !isReadyToLunch();
+        }
+    }
+}
+
+function initUnlockBtn() {
+    unlockBtn.onclick = () => {
+        if (passField.value === PASSWORD) {
+            setDisabled(unlockBtn, true)
+            setDisabled(passField, true)
+            setDisabled(checkBtns)
+            setDisabled(levers)
+        }
+    };
+}
+
+function disableControls() {
+    setDisabled(checkBtns, true)
+    setDisabled(levers, true)
+    setDisabled(lunchBtn, true)
+}
+
+
+initUnlockBtn();
+initCheckBtns();
+initLevers();
+disableControls();
